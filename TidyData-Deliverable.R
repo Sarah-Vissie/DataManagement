7f695c8b22e98345055a9a7f736fed2c588debf6
Sarah-Vissie <VSSSAR003@myuct.ac.za>
2024-02-19 21:57:08 +0200

    Tidy Data was Pivoted

--- TidyData-Deliverable.R
+++ TidyData-Deliverable.R
@@ -18,21 +18,34 @@ Batisdat_tidy <- Batisdat %>%
 #Reformatting dur into seconds; removing the date that was bought over from excel
 
 class(Batisdat_tidy$dur)
+class("succ")
+class("sex")
+
 
 #The Data type of dur is POSIXct and POSIXt
 
+Batisdat_tidy$dur_seconds <- second(Batisdat_tidy$dur)
 
+Batisdat_t_pivot <- Batisdat_tidy %>%
+  select("succ", "dur_seconds", "sex") %>%
+  group_by("sex") %>%
+  pivot_longer(cols = c("succ", "dur_seconds"), 
+               names_to = "Variable", values_to = "Values") %>%
 
-Batisdat_tidy_dur <- as.difftime(Batisdat_tidy$dur, units = "secs")
+  ggplot() +
+  geom_boxplot(aes(y = "succ", x = "dur_seconds")) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  facet_wrap(vars("sex"), scales = "free")
 
 #Pivoting data into wide format
 
+
 Batisdat_pivot <- Batisdat_tidy %>%
-  select("succ", "dur", "sex") %>%
-  pivot_longer(cols = c("succ", "dur", "sex"),
-               names_to = "Variable", values_to = "Values")
-
-
+  select("succ", "dur_seconds", "sex") %>%
+  pivot_longer(cols = c("succ", "dur_seconds", "sex"), 
+               names_to = "Variable", values_to = "Values") %>%
+  
+  
 Batisdat_pivot <- Batisdat %>%
   select("Duration :", "Success:", "Male/Female:") %>%
   pivot_longer(cols = c("Duration :", "Success:", "Male/Female:"), 
