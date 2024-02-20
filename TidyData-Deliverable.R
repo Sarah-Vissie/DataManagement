f182f87645b199af71b2f25624acea1350e11590
Sarah-Vissie <VSSSAR003@myuct.ac.za>
2024-02-20 09:36:29 +0200

    Finally got stuff almost right into a graph but now just need help to make the graph "graph"

--- TidyData-Deliverable.R
+++ TidyData-Deliverable.R
@@ -17,14 +17,8 @@ Batisdat_tidy <- Batisdat %>%
 
 #Reformatting dur into seconds; removing the date that was bought over from excel
 
-class(Batisdat_tidy$dur)
-class("succ")
-class("sex")
 
-
-#The Data type of dur is POSIXct and POSIXt
-
-Batisdat_tidy$dur_seconds <- second(Batisdat_tidy$dur)
+Batisdat_tidy$dur <- as.numeric(format(as.POSIXct(Batisdat_tidy$dur, format="%Y-%m-%d %H:%M:%S"), "%S"))
 
 Batisdat_t_pivot <- Batisdat_tidy %>%
   select("succ", "dur_seconds", "sex") %>%
@@ -32,10 +26,13 @@ Batisdat_t_pivot <- Batisdat_tidy %>%
   pivot_longer(cols = c("succ", "dur_seconds"), 
                names_to = "Variable", values_to = "Values") %>%
 
+
+
   ggplot() +
   geom_boxplot(aes(y = "succ", x = "dur_seconds")) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   facet_wrap(vars("sex"), scales = "free")
+Batisdat_t_pivot
 
 #Pivoting data into wide format
 
